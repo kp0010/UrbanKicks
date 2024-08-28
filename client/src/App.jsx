1eb6f5cde7e7cd15b7e5d8af85c4e62cc82c6b0a
kp0010 <kartikcr750@gmail.com>
2024-08-28 23:27:51 +0530

    Conv HomePage to React JS

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 import { Navbar } from './Components/Navbar/Navbar';
+import { Home } from './Components/HomePage/Home';
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <h1>REST OF THE FUCKING SITE</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/nav" element={<Navbar />} />
+        </Routes>
+        <h1>REST OF THE FUCKING SITE</h1>
+      </Router>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.js'
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App/>
+    <App />
   </React.StrictMode>
 );
