4e50450239d5516a219d38f9f7002aa1fa2111ee
kp0010 <kartikcr750@gmail.com>
2024-08-29 17:52:39 +0530

    Clean Repo

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,7 +16,6 @@ function App() {
           <Route exact path="/" element={<Home />} />
           <Route path="/nav" element={<Navbar />} />
         </Routes>
-        <h1>REST OF THE FUCKING SITE</h1>
       </Router>
     </div>
   );
