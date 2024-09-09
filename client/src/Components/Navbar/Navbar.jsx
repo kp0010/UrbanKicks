c1ec5ba1432420f0e055bf1fb65c8fd84657d711
kp0010 <kartikcr750@gmail.com>
2024-09-09 18:36:00 +0530

    Fix Login

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -6,21 +6,19 @@ import { NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <div className="navbar">
-      <div class="nav">
-        <a href="#"><img src={logo} class="logo"></img></a>
-        <ul class="list">
-          <a> <li><NavLink to="/nav">Mens</NavLink></li> </a>
-          <a> <li>Womens</li> </a>
-          <a> <li>Kids</li> </a>
-        </ul>
-        <ul class="icons">
-          <a><li><i class="bi bi-search"></i></li></a>
-          <a><li><i class="bi bi-bag"></i></li></a>
-          <a><li><NavLink to="/Login"><i class="bi bi-person"></i></NavLink></li></a>
-          <a><li><i class="bi bi-heart"></i></li></a>
-        </ul>
-      </div>
+    <div class="nav">
+      <a href="#"><img src={logo} class="logo"></img></a>
+      <ul class="list">
+        <a> <li><NavLink to="/nav">Mens</NavLink></li> </a>
+        <a> <li>Womens</li> </a>
+        <a> <li>Kids</li> </a>
+      </ul>
+      <ul class="icons">
+        <a><li><i class="bi bi-search"></i></li></a>
+        <a><li><i class="bi bi-bag"></i></li></a>
+        <a><li><NavLink to="/Login"><i class="bi bi-person"></i></NavLink></li></a>
+        <a><li><i class="bi bi-heart"></i></li></a>
+      </ul>
     </div>
   )
 }
