c89e982c6f5d62885c39f8ad28d2ab65ac76d53f
Sahilpatil135 <sahilrp2005@gmail.com>
2024-09-14 21:22:19 +0530

    Pages updated

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -9,13 +9,13 @@ export const Navbar = () => {
     <div class="nav">
       <NavLink to="/"><img src={logo} class="logo"></img></NavLink>
       <ul class="list">
-        <li><NavLink to="/mens">Mens</NavLink></li>
-        <li><NavLink to="/womens">Womens</NavLink></li>
+        <li><NavLink to="/men">Men</NavLink></li>
+        <li><NavLink to="/women">Women</NavLink></li>
         <li><NavLink to="/kids">Kids</NavLink></li>
       </ul>
       <ul class="icons">
         <li><i class="bi bi-search"></i></li>
-        <li><i class="bi bi-bag"></i></li>
+        <li><NavLink to="/cart"><i class="bi bi-bag"></i></NavLink></li>
         <li><NavLink to="/Login"><i class="bi bi-person"></i></NavLink></li>
         <li><i class="bi bi-heart"></i></li>
       </ul>
