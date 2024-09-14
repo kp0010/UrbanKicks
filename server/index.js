9347755c087a15762ccbbf7ce32149f794feb9b8
kp0010 <kartikcr750@gmail.com>
2024-09-14 06:41:07 +0530

    Add Persistent Login with Sessions

--- server/index.js
+++ server/index.js
@@ -1,13 +1,41 @@
-import express, { response } from "express"
+import express from "express"
 import pg from "pg"
 import cors from "cors"
+import passport from "passport"
+import LocalStratergy from "passport-local"
+import session from "express-session"
 
-const app = express();
-app.use(cors())
-app.use(express.json())
-const { Pool } = pg;
+import SQLiteStoreFactory from "connect-sqlite3"
+const SQLiteStore = SQLiteStoreFactory(session)
 
 const PORT = 8080;
+const app = express();
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}))
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use(session({
+    secret: "insertsecrethere",
+    resave: false,
+    saveUninitialized: false,
+    store: new SQLiteStore({ db: 'session.db', dir: './db/' }),
+    cookie: {
+        domain: "localhost",
+        httpOnly: true,
+        secure: false,
+        maxAge: 72 * 60 * 60 * 1000 //72 hrs exp
+    }
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+const { Pool } = pg;
 
 const dbConfig = {
     user: 'postgres',
@@ -24,42 +52,62 @@ db.connect(function(err) {
     console.log("Connected!");
 });
 
+passport.use(new LocalStratergy(function verify(username, password, cb) {
+    db.query("SELECT * FROM users WHERE username=$1", [username], (error, res) => {
+        if (error) { throw error }
 
-app.get("/", (request, response) => {
-    db.query("SELECT * FROM cart", (error, results) => {
-        if (error) {
-            throw error
+        if (!res.rows) { console.log("Invalid Username"); return cb(null, false, { message: "Username Invalid" }) }
+
+        // console.log(username, password, res.rows[0], "info")
+
+        if (res.rows[0].password === password) {
+            console.log("Password Verified")
+            return cb(null, res.rows[0])
+        } else {
+            console.log("Invalid Password")
+            return cb(null, false, { message: "Incorect Password" })
         }
-        return response.status(200).json(results.rows)
     })
-});
+}))
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.username)
+})
 
 
-app.post("/Login/", (request, response) => {
-    const { username, password } = request.body
-    console.log(username, password)
-
-    db.query("SELECT * FROM users where userid = $1", [username], (error, res) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json({
-            status: res.rows.length && (password == res.rows[0].password) ?
-                "Login Successfull" : "Login Unsuccessfull",
-            login: res.rows.length && password == res.rows[0].password,
-            userExists: res.rows.length && (res.rows.length != 0)
-        })
+passport.deserializeUser(function(username, cb) {
+    db.query("SELECT * FROM users where username = $1", [username], (error, res) => {
+        if (error) { throw error }
+        return cb(null, res.rows[0])
     })
-});
+})
 
 
-app.get("/cart/:userId", (request, response) => {
-    db.query("SELECT * FROM cart where userid = $1", [request.params.userId], (error, res) => {
-        if (error) {
-            throw error
-        }
-        return response.json(res.rows)
-    })
+app.post("/login", (req, res, next) => {
+    passport.authenticate('local', (err, user, info) => {
+        if (err) return next(err);
+        if (!user) return res.status(401).json({ message: info.message });
+
+        req.logIn(user, (err) => {
+            if (err) return next(err);
+            return res.status(200).json({ login: true, message: "Login Successful", user })
+        });
+    })(req, res, next)
+})
+
+app.get("/user", (req, res) => {
+    if (req.isAuthenticated()) {
+        return res.json({ authenticated: true, user: req.user });
+    } else {
+        return res.json({ authenticated: false })
+    }
+})
+
+app.post("/logout", (req, res) => {
+    req.logout(err => {
+        if (err) return res.status(500).json({ message: 'Logout error' });
+        res.json({ message: 'Logged out' });
+    });
 });
 
 
