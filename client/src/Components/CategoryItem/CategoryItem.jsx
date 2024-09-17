3dd3111cc5473ef53b7be50b8d263b808a3900e4
Sahilpatil135 <sahilrp2005@gmail.com>
2024-09-17 18:39:52 +0530

    Men Products displayed

--- client/src/Components/CategoryItem/CategoryItem.jsx
+++ client/src/Components/CategoryItem/CategoryItem.jsx
@@ -4,7 +4,33 @@ import "./CategoryItem.css"
 export const CategoryItem = (props) => {
   return (
     <div className='category-item'>
-        <img src={props.image} alt="." />
+        <a href="/">
+          <div>
+            <img src={props.image} alt="." />
+          </div>
+          <div >
+            <p>{props.name} : {props.subtitle}</p>
+          </div>
+          <div className="category-item-prices">
+            <div className="category-item-price-new">
+              ${props.new_price}
+            </div>
+            <div className="category-item-price-old">
+              ${props.old_price}
+            </div>
+          </div>
+        </a>
+        {/*<img src={props.image} alt="." />
+        <p>{props.name}</p>
+        <div className="category-item-price">
+          <div className="category-item-price-new">
+            ${props.new_price}
+          </div>
+          <div className="category-item-price-old">
+            ${props.old_price}
+          </div>
+        </div>*/}
+
     </div>
   )
 }
--- client/src/Pages/ShopCategory.jsx
+++ client/src/Pages/ShopCategory.jsx
@@ -1,22 +1,35 @@
 import React, { useContext } from 'react'
 import "./CSS/ShopCategory.css"
-import { ShopContext } from '../Context/ShopContext'
-import { Item } from '../Components/Item/Item';
+import { ShopContext } from '../Context/ShopContext';
+import { CategoryItem } from '../Components/CategoryItem/CategoryItem';
+import banner_1 from "../Components/Assets/HomeAssets/banner1.png"
 
 export const ShopCategory = (props) => {
     const {men_sandals} = useContext(ShopContext);
   return (
     <div className="shop-category">
+      <div className="shop-category-banner">
+        <img src={banner_1} alt="." />
+      </div>
+      <div className="shop-category-sort">
+        <h4>Sorting Remaining</h4>
+      </div>
+      <div className="shop-category-main">
+        <div className="shop-category-filters">
+          <h3>Filters Remaining</h3>
+        </div>
         <div className="shop-category-products">
           {men_sandals.map((item, i) => {
             if (props.category===item.gender){
-              return <Item key={i} id={item.id} name={item.title} image={item.img} new_price={item.price}/>
+              return <CategoryItem key={i} id={item.id} name={item.title} subtitle={item.subtitle} image={item.img} new_price={item.price}/>
             }
             else{
               return null;
             }
           })}
         </div>
+      </div>
+        
     </div>
   )
 }
