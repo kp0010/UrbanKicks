3ee85e004ba62978494f1713512fc5b23cdcc84c
Sahilpatil135 <sahilrp2005@gmail.com>
2024-09-24 18:35:57 +0530

    Add to cart button added

--- client/src/Components/CategoryItem/CategoryItem.jsx
+++ client/src/Components/CategoryItem/CategoryItem.jsx
@@ -4,19 +4,22 @@ import "./CategoryItem.css"
 export const CategoryItem = (props) => {
   return (
     <div className='category-item'>
-        <a href="/">
+        <a href="/product">
           <div>
             <img src={props.image} alt="." />
           </div>
+          <button className="category-item-addtocart">
+            ADD TO CART
+          </button>
           <div >
             <p>{props.name} : {props.subtitle}</p>
           </div>
           <div className="category-item-prices">
             <div className="category-item-price-new">
-              ${props.new_price}
+              Rs {props.new_price}
             </div>
             <div className="category-item-price-old">
-              ${props.old_price}
+              Rs {props.old_price}
             </div>
           </div>
         </a>
--- client/src/Components/Item/Item.jsx
+++ client/src/Components/Item/Item.jsx
@@ -7,16 +7,17 @@ export const Item = (props) => {
       <div>
         <img src={props.image} alt="."/>
       </div>
+      <button className="item-addtocart">ADD TO CART</button>
       <div >
         <p>{props.name} : {props.subtitle}</p>
       </div>  
       
       <div className="item-prices">
           <div className="item-price-new">
-              ${props.new_price}
+              Rs {props.new_price}
           </div>
             <div className="item-price-old">
-              ${props.old_price}
+              Rs {props.old_price}
           </div>
       </div>
     </div>
