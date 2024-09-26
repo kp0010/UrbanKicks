0fc8bb836d76c5ce83cdf1963b49609029af5eaf
30mkyaa <30mkyaa@gmail.com>
2024-09-26 15:05:15 +0530

    Shop by collection Done

--- client/src/Components/ShopByCollection/ShopByCollection.jsx
+++ client/src/Components/ShopByCollection/ShopByCollection.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import "./ShopByCollection.css"
+import ShopByCollectionWomen from "../Assets/HomeAssets/ShopbyCollection-Women.png"
+import ShopByCollectionMen from "../Assets/HomeAssets/ShopbyCollection-Men.png"
+import ShopByCollectionKid from "../Assets/HomeAssets/ShopbyCollection-Kid.png"
 
 export const ShopByCollection = () => {
   return (
     <div>
       <div className="collection">
-        <h2 className= "heading">Shop By Collection</h2>
+      <h2 className= "heading">Shop By Collection</h2>
+      <div className='banners'>
+      <img src={ShopByCollectionWomen} alt="." />
+      <img src={ShopByCollectionMen} alt="." />
+      <img src={ShopByCollectionKid} alt="." />
+      </div>
       </div>
     </div>
   )
