48be4a524cb2149f0fc77e0ecc367aa98630a1b9
Sahilpatil135 <sahilrp2005@gmail.com>
2024-10-12 18:19:48 +0530

    checkout page done

--- client/src/Components/BestSellers/BestSellers.jsx
+++ client/src/Components/BestSellers/BestSellers.jsx
@@ -5,7 +5,7 @@ import new_arrival from "../Assets/HomeAssets/bestsellers1.png"
 import data_product from '../Assets/ProductData/new_arrivals.js'
 import { Item } from '../Item/Item'
 
-export const BestSellers = (props) => {
+export const BestSellers = () => {
   const { new_arrivals } = useShop();
   return (
     <div>
@@ -15,9 +15,10 @@ export const BestSellers = (props) => {
 
         <div className="Best-Sellers-item">
           {new_arrivals.map((item, i) => {
-            return <Item key={i} id={item.id} name={item.title} subtitle={item.subtitle} image={item.image} new_price={item.price} />
+            return <Item key={i} id={item.id} name={item.title} subtitle={item.subtitle} image={item.img} new_price={item.price} />
           })}
         </div>
+
         <div className="view-product">
           {/*Add link to product page pending*/}
           <a href="">VIEW PRODUCTS
@@ -26,14 +27,6 @@ export const BestSellers = (props) => {
             </svg>
           </a>
         </div>
-      </div>
-      <div className="view-product">
-        {/*Add link to product page pending*/}
-        <a href="">VIEW PRODUCTS
-          <svg width="20" height="20" viewBox="0 0 24 24" fill="none">
-            <path d="M 9 2.5 L 16.5 10 L 9 17.5" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
-          </svg>
-        </a>
       </div>
     </div>
   )
--- client/src/Pages/Checkout.jsx
+++ client/src/Pages/Checkout.jsx
@@ -1,7 +1,73 @@
 import React from 'react'
+import "./CSS/Checkout.css"
+import { useShop } from '../Context/ShopContext'
 
 export const Checkout = () => {
+  const { price } = useShop()
   return (
-    <div>Checkout</div>
+    <div className="checkout">
+      <div className="checkout-head">Checkout</div>
+      <div className="checkout-main">
+        <div className="checkout-left">
+          <div className="checkout-left-shipping-content">
+            <div className="checkout-left-shipping">
+              <p>1.SHIPPING</p>
+            </div>
+            <p className="checkout-address-label">Add New Address</p>
+            <div className="checkout-left-address">
+              <div className="checkout-row">
+                <input type="text" id="firstname" placeholder="FIRST NAME"/>
+                <input type="text" id="lastname" placeholder="LAST NAME"/>
+              </div>
+              <div className="checkout-row">
+                <input type="text" id="address1" placeholder="ADDRESS 1"/>
+              </div>
+              <div className="checkout-row">
+                <input type="text" id="address2" placeholder="ADDRESS 2 (OPTIONAL)"/>
+              </div>
+              <div className="checkout-row">
+                <input type="number" id="postalcode" placeholder="POSTAL CODE"/>
+                <input type="text" id="state" placeholder="STATE"/>
+              </div>
+              <div className="checkout-row">
+                <input type="text" id="city" placeholder="CITY"/>
+                <input type="text" id="country" placeholder="COUNTRY"/>
+              </div>
+              <div className="checkout-row">
+                <input type="tel" id="phonenumber" placeholder="PHONE NUMBER"/>
+              </div>
+              <div className="checkout-buttons">
+                <button className="button-save">SAVE</button>
+                <button className="button-cancel">CANCEL</button>
+              </div>
+            </div>
+          </div>
+          <div className="checkout-left-payment-content">
+            <p className="checkout-payment">2.PAYMENT</p>
+            <p className="payment-delivery-label">Pay On Delivery</p>
+          </div>
+        </div>
+        <div className="checkout-right">
+          <div className="checkout-right-bill">
+            <h4>Price Details</h4>
+            <hr />
+            <div className="checkout-right-subtotal">
+              <div className="checkout-right-subtotal-left">
+                <p>Subtotal</p>
+                <p>Shipping</p>
+                <p>Total</p>
+              </div>
+              <div className="checkout-right-subtotal-right">
+                <p>₹{price}</p>
+                <p>₹{price !== 0 ? "300" : "0"}</p>
+                <p>₹{price !== 0 ? price + 300 : "0"}</p>
+              </div>
+            </div>
+            <hr />
+            <p className="tax-tag">All prices are including tax*</p>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
