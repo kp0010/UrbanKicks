0b00faa0df60d9bb776ecd9e13bfaefe50feeafb
Sahilpatil135 <sahilrp2005@gmail.com>
2024-10-20 17:54:22 +0530

    Review Form Updated

--- client/src/Components/Reviews/Reviews.jsx
+++ client/src/Components/Reviews/Reviews.jsx
@@ -1,26 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Reviews.css"
 
 export const Reviews = () => {
+  const [ rating, setRating ] = useState(0);
+
+  const handleStarRating =(index) => {
+    setRating(index);
+  }
   return (
     <div className="reviews">
       <div className="reviews-customerReviews">
         <div className="reviews-customerReviews-head">
           <h2>Customer Reviews</h2>
         </div>
-        <div className="reviews-customerReviews-ratings"></div>
-        <div className="reviews-customerReviews-writeReview-button">
-          <button>Write a Review</button>
+        <div className="reviews-customerReviews-content">
+          <div className="reviews-customerReviews-ratings"></div>
+          <div className="reviews-customerReviews-writeReview-button">
+            <button>Write a Review</button>
+          </div>
         </div>
+        
       </div>
       <hr />
       <div className="reviews-writeReview">
         <form>
           <div className="reviews-writeReview-fieldset">
-            <h4>Write a Review</h4>
+            <p className="write-review">Write a Review</p>
           </div>
           <div className="reviews-writeReview-fieldset">
             <label>Rating</label>
+            <div className="star-rating">
+              {[1, 2, 3, 4, 5].map((star,index) => (
+                <i key={index} className={`bi ${rating >= star ? 'bi-star-fill text-warning' : 'bi-star text-black'}`} onClick={()=> handleStarRating(star)}></i>
+              ))}
+            </div>
             {/* add span tag for star */}
           </div>
           <div className="reviews-writeReview-fieldset">
@@ -40,10 +53,10 @@ export const Reviews = () => {
             <input type="email" placeholder="Enter your email" />
           </div>
           <div className="reviews-writeReview-fieldset">
-            <p>How we use your data: We’ll only contact you about the review you left, and only if necessary. By submitting your review, you agree to Judge.me’s terms, privacy and content policies.</p>
+            <p className="random-data">How we use your data: We’ll only contact you about the review you left, and only if necessary. By submitting your review, you agree to Judge.me’s terms, privacy and content policies.</p>
           </div>
-          <div className="reviews-writeReview-fieldset">
-            <a href="" role="button">Cancel Reivew</a>
+          <div className="reviews-writeReview-fieldset-submit">
+            <a href="" role="button">Cancel Review</a>
             <input type="submit" value="Submit Review" />
           </div>
           <hr />
